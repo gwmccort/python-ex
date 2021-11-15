80cbec0922d1867f8a1320266c292481ecd495b3
Glen McCort <gwmccort@gmail.com>
2021-11-15 16:04:38 -0600

    simple changes

--- evernote/sax-test.py
+++ evernote/sax-test.py
@@ -20,15 +20,27 @@ testXml = """<?xml version="1.0" encoding="UTF-8"?>
 """
 
 
+class SaxHandler(xml.sax.ContentHandler):
+
+    def __init__(self):
+        self.noteCount = 0
+        self.isInNote = False
+        self.isInTitle = False
+
+    # Handle startElement
+    def startElement(self, tagName, attrs):
+        print(tagName)
+
+
 def main():
     print("Start(main)...")
 
     # stream = StringIO(testXml)
     # print(stream.read())
 
-    # # create a new content handler for the SAX parser
-    # handler = SaxHandler()
-    # xml.sax.parseString(testXml, handler)
+    # create a new content handler for the SAX parser
+    handler = SaxHandler()
+    xml.sax.parseString(testXml, handler)
 
     print("End: main()")
 
