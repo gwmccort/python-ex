bb750ac0dbd5cd26377f5d8dc20c18318fab779c
Glen McCort <gwmccort@gmail.com>
2021-12-28 11:47:38 -0600

    file walker

---
+++ io/README.md
@@ -0,0 +1,6 @@
+python i/o examples
+
+* 2files.py - open 2 files in a with
+* file-read-write.py
+* file-walk.py - read a whole directory tree
+* gwm.py
---
+++ io/file-walk.py
@@ -0,0 +1,58 @@
+# walk file dir example
+# from: https://stackoverflow.com/questions/2212643/python-recursive-folder-read
+##########################
+
+import os
+import sys
+
+# walk_dir = sys.argv[1]
+walk_dir = "."
+
+print('walk_dir = ' + walk_dir)
+
+# If your current working directory may change during script execution, it's recommended to
+# immediately convert program arguments to an absolute path. Then the variable root below will
+# be an absolute path as well. Example:
+# walk_dir = os.path.abspath(walk_dir)
+print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+
+for root, subdirs, files in os.walk(walk_dir):
+    print('--\nroot = ' + root)
+    list_file_path = os.path.join(root, 'my-directory-list.txt')
+    print('list_file_path = ' + list_file_path)
+
+    with open(list_file_path, 'wb') as list_file:
+        for subdir in subdirs:
+            print('\t- subdirectory ' + subdir)
+
+        for filename in files:
+            file_path = os.path.join(root, filename)
+
+            print('\t- file %s (full path: %s)' % (filename, file_path))
+
+            with open(file_path, 'rb') as f:
+                f_content = f.read()
+                list_file.write(('The file %s contains:\n' %
+                                filename).encode('utf-8'))
+                list_file.write(f_content)
+                list_file.write(b'\n')
+
+# consice way to do it
+######################
+# for currentpath, folders, files in os.walk('.'):
+#     for file in files:
+#         print(os.path.join(currentpath, file))
+
+
+# If you didn't know, the with statement for files is a shorthand
+#################################################################
+# with open('filename', 'rb') as f:
+#     dosomething()
+
+# # is effectively the same as
+
+# f = open('filename', 'rb')
+# try:
+#     dosomething()
+# finally:
+#     f.close()
